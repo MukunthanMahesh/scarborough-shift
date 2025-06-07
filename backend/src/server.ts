59f0e60bec7f1cf9d57c762accc52d95f6e76caf
Mukunthan Mahesh <mukunthanmahesh@gmail.com>
2025-06-07 14:34:45 -0400

    chore: [ECS-42] initialize root route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,8 +7,14 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// Health check route
 app.get("/ping", (_, res) => {
   res.send("pong");
+});
+
+// Root route
+app.get("/", (_, res) => {
+  res.send("Welcome to the Scarborough Shift API");
 });
 
 app.listen(PORT, () => {
